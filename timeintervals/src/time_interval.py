ea509173ef2360cd88a7132c34de97f8c7cb94e3
RyanDoesMath <rfolks92@gmail.com>
2025-04-11 10:17:22 -0400

    Add a constructor called from_strings that can create TimeIntervals by parsing strings with a given format.

--- timeintervals/src/time_interval.py
+++ timeintervals/src/time_interval.py
@@ -1,5 +1,10 @@
 """A module defining the TimeInterval class, a construct for working with intervals of time."""
 
+from ._custom_exceptions import (
+    InvalidTimeIntervalError,
+    TimeFormatMismatchError,
+    UnconvertedDataError,
+)
 from datetime import datetime, timedelta
 from pydantic import BaseModel, Field, model_validator
 from typing_extensions import Self
@@ -7,7 +12,7 @@ from typing_extensions import Self
 
 class TimeInterval(BaseModel):
     """The TimeInterval class represents a set of time between a start and an end.
-    
+
     TimeIntervals are immutable by default, any operation on them results in a new TimeInterval.
     """
 
@@ -18,7 +23,7 @@ class TimeInterval(BaseModel):
     def check_end_gt_start(self) -> Self:
         """Checks to make sure end is greater than start."""
         if self.end < self.start:
-            raise ValueError(
+            raise InvalidTimeIntervalError(
                 f"Cannot construct TimeInterval: {self.end} is greater than {self.start}."
             )
         return self
@@ -37,9 +42,46 @@ class TimeInterval(BaseModel):
             kwargs["end"] = args[1]
         super().__init__(**kwargs)
 
+    @classmethod
+    def from_strings(cls, start_str: str, end_str: str, time_format: str) -> Self:
+        """Creates a time interval by parsing strings.
+
+        Args:
+            start (str):
+                The start for the TimeInterval.
+            end (str):
+                The end for the TimeInterval.
+            time_format (str):
+                The format for the time. See the 1989 C standard for how to format this
+                string. This is plugged directly into strptime.
+
+        Returns:
+            A TimeInterval with the start and end corresponding to the start_str and
+            end_str parsed by time_format.
+
+        Raises:
+            ValueError:
+                If time_format does not match the format of start_str, or if unconverted
+                data remains in the string.
+        """
+        try:
+            start: datetime = datetime.strptime(start_str, time_format)
+            end: datetime = datetime.strptime(end_str, time_format)
+            return TimeInterval(start, end)
+        except ValueError as e:
+            error_message: str = str(e)
+            if "does not match format" in error_message:
+                raise TimeFormatMismatchError(e)
+            elif "unconverted data" in str(e):
+                raise UnconvertedDataError(e)
+            else:
+                raise e
+        except Exception as e:
+            raise e
+
     def time_elapsed(self) -> timedelta:
         """The amount of time between this TimeInterval's start and end.
-        
+
         Returns:
             A timedelta containing the amount of time between start and end.
         """
