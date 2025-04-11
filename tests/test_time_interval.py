6aeaa0fbf1992da38173b3e5975935c4d144543e
RyanDoesMath <rfolks92@gmail.com>
2025-04-11 09:40:51 -0400

    Add test for time_elapsed.
    Also cleaned up some of the test logic.

--- tests/test_time_interval.py
+++ tests/test_time_interval.py
@@ -6,10 +6,14 @@ import pytest
 from timeintervals import TimeInterval
 
 
+ONE_MINUTE: timedelta = timedelta(minutes=1)
+
+
 def test_normal_construction():
     """Tests construction with valid data in the way users would expect to construct objects."""
-    start: datetime = datetime.now() - timedelta(minutes=1)
-    end: datetime = datetime.now()
+    now: datetime = datetime.now()
+    start: datetime = now - ONE_MINUTE
+    end: datetime = now
 
     # Test both positional and keyword argument construction because of pydantic.
     TimeInterval(start=start, end=end)
@@ -18,8 +22,19 @@ def test_normal_construction():
 
 def test_end_before_start():
     """Tests that construction raises an exception when end is before start."""
-    start: datetime = datetime.now()
-    end: datetime = datetime.now() - timedelta(minutes=1)
+    now: datetime = datetime.now()
+    start: datetime = now
+    end: datetime = now - ONE_MINUTE
 
     with pytest.raises(ValidationError, match="is greater than"):
         TimeInterval(start, end)
+
+
+def test_time_elapsed():
+    """Tests the time_elapsed method."""
+    now: datetime = datetime.now()
+    start: datetime = now - ONE_MINUTE
+    end: datetime = now
+
+    interval: TimeInterval = TimeInterval(start, end)
+    assert(interval.time_elapsed() == ONE_MINUTE)
