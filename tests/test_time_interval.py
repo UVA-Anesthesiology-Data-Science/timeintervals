a0786cdb3bfd29ca35b2616a8c692e8495cbbba9
RyanDoesMath <rfolks92@gmail.com>
2025-04-11 08:41:31 -0400

    Create test for normal usage of TimeInterval.

--- tests/test_time_interval.py
+++ tests/test_time_interval.py
@@ -1,5 +1,14 @@
 """Tests the TimeInterval class."""
 
+from datetime import datetime, timedelta
 import pytest
 from timeintervals import TimeInterval
 
+
+def test_normal_construction():
+    start: datetime = datetime.now() - timedelta(minutes=1)
+    end: datetime = datetime.now()
+    
+    # test positional and keyword arguments because of pydantic.
+    TimeInterval(start=start, end=end)
+    TimeInterval(start, end)
