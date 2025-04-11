3739954fab8db2ddb0c0332937d39466a9f7a36f
RyanDoesMath <rfolks92@gmail.com>
2025-04-11 08:46:12 -0400

    Add test for exceptions when end is before start.

--- tests/test_time_interval.py
+++ tests/test_time_interval.py
@@ -1,6 +1,7 @@
 """Tests the TimeInterval class."""
 
 from datetime import datetime, timedelta
+from pydantic_core._pydantic_core import ValidationError
 import pytest
 from timeintervals import TimeInterval
 
@@ -8,7 +9,15 @@ from timeintervals import TimeInterval
 def test_normal_construction():
     start: datetime = datetime.now() - timedelta(minutes=1)
     end: datetime = datetime.now()
-    
-    # test positional and keyword arguments because of pydantic.
+
+    # Test both positional and keyword argument construction because of pydantic.
     TimeInterval(start=start, end=end)
     TimeInterval(start, end)
+
+
+def test_end_before_start():
+    start: datetime = datetime.now()
+    end: datetime = datetime.now() - timedelta(minutes=1)
+
+    with pytest.raises(ValidationError, match="is greater than"):
+        TimeInterval(start, end)
