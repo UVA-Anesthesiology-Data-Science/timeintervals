5e22399f1f258ad2fe45047b16de30ee1fb42eb1
RyanDoesMath <rfolks92@gmail.com>
2025-04-14 14:11:06 -0400

    Add a test for is_empty.

--- tests/test_time_set.py
+++ tests/test_time_set.py
@@ -2,3 +2,19 @@
 
 import pytest
 from datetime import datetime, timedelta
+from timeintervals import (
+    TimeFormatMismatchError,
+    TimeInterval,
+    TimeSet,
+    UnconvertedDataError,
+)
+
+ONE_MINUTE: timedelta = timedelta(minutes=1)
+
+def test_is_empty():
+    """Tests the is_empty method."""
+    now: datetime = datetime.now()
+    empty_time_set: TimeSet = TimeSet([])
+    non_empty_time_set: TimeSet = TimeSet([TimeInterval(now - ONE_MINUTE, now)])
+    assert empty_time_set.is_empty()
+    assert not non_empty_time_set.is_empty()
