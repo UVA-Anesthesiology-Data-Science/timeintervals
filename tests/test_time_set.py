885b610b8028803ac9a550c6a0f2a98938abdd3d
RyanDoesMath <rfolks92@gmail.com>
2025-04-14 17:50:50 -0400

    Format test_time_set.py

--- tests/test_time_set.py
+++ tests/test_time_set.py
@@ -2,15 +2,12 @@
 
 import pytest
 from datetime import datetime, timedelta
-from timeintervals import (
-    TimeFormatMismatchError,
-    TimeInterval,
-    TimeSet,
-    UnconvertedDataError,
-)
+from timeintervals import TimeInterval, TimeSet
+
 
 ONE_MINUTE: timedelta = timedelta(minutes=1)
 NOW: datetime = datetime.now()
+
 
 def test_is_empty():
     """Tests the is_empty method."""
@@ -19,31 +16,40 @@ def test_is_empty():
     assert empty_time_set.is_empty()
     assert not non_empty_time_set.is_empty()
 
+
 def test_add_time_interval_to_time_set():
     """Tests the __add__ method by adding a TimeInterval to a TimeSet."""
     new_time_interval: TimeInterval = TimeInterval(NOW, NOW + ONE_MINUTE)
-    pre_add_time_set: TimeSet = TimeSet([
-        TimeInterval(NOW - 3*ONE_MINUTE, NOW - 2*ONE_MINUTE),
-        TimeInterval(NOW - ONE_MINUTE, NOW + ONE_MINUTE),
-    ])
-    post_add_time_set: TimeSet = TimeSet([
-        TimeInterval(NOW - 3*ONE_MINUTE, NOW - 2*ONE_MINUTE),
-        TimeInterval(NOW - ONE_MINUTE, NOW + ONE_MINUTE),
-        TimeInterval(NOW, NOW + ONE_MINUTE)
-    ])
+    pre_add_time_set: TimeSet = TimeSet(
+        [
+            TimeInterval(NOW - 3 * ONE_MINUTE, NOW - 2 * ONE_MINUTE),
+            TimeInterval(NOW - ONE_MINUTE, NOW + ONE_MINUTE),
+        ]
+    )
+    post_add_time_set: TimeSet = TimeSet(
+        [
+            TimeInterval(NOW - 3 * ONE_MINUTE, NOW - 2 * ONE_MINUTE),
+            TimeInterval(NOW - ONE_MINUTE, NOW + ONE_MINUTE),
+            TimeInterval(NOW, NOW + ONE_MINUTE),
+        ]
+    )
     assert (pre_add_time_set + new_time_interval) == post_add_time_set
 
 
 def test_add_time_set_to_time_set():
     """Tests the __add__ method by adding a TimeInterval to a TimeSet."""
     new_time_set: TimeSet = TimeSet([TimeInterval(NOW, NOW + ONE_MINUTE)])
-    pre_add_time_set: TimeSet = TimeSet([
-        TimeInterval(NOW - 3*ONE_MINUTE, NOW - 2*ONE_MINUTE),
-        TimeInterval(NOW - ONE_MINUTE, NOW + ONE_MINUTE),
-    ])
-    post_add_time_set: TimeSet = TimeSet([
-        TimeInterval(NOW - 3*ONE_MINUTE, NOW - 2*ONE_MINUTE),
-        TimeInterval(NOW - ONE_MINUTE, NOW + ONE_MINUTE),
-        TimeInterval(NOW, NOW + ONE_MINUTE)
-    ])
+    pre_add_time_set: TimeSet = TimeSet(
+        [
+            TimeInterval(NOW - 3 * ONE_MINUTE, NOW - 2 * ONE_MINUTE),
+            TimeInterval(NOW - ONE_MINUTE, NOW + ONE_MINUTE),
+        ]
+    )
+    post_add_time_set: TimeSet = TimeSet(
+        [
+            TimeInterval(NOW - 3 * ONE_MINUTE, NOW - 2 * ONE_MINUTE),
+            TimeInterval(NOW - ONE_MINUTE, NOW + ONE_MINUTE),
+            TimeInterval(NOW, NOW + ONE_MINUTE),
+        ]
+    )
     assert (pre_add_time_set + new_time_set) == post_add_time_set
