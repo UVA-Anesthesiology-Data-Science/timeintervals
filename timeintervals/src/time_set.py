75015a6d705170d9c21705321f1998b1ae50bc40
RyanDoesMath <rfolks92@gmail.com>
2025-04-14 08:35:56 -0400

    Expand on the class docstring for TimeSet.

--- timeintervals/src/time_set.py
+++ timeintervals/src/time_set.py
@@ -5,7 +5,17 @@ from typing import List, Union
 from typing_extensions import Self
 
 class TimeSet:
-    """A set of TimeIntervals that defines set-like operations."""
+    """A set of TimeIntervals that defines set-like operations.
+    
+    This class expands on the functionality of TimeInterval.
+    TimeInterval can only represent sets of time that are contiguous. For some
+    applications this is all that is necessary, but for other applications
+    (such as those that need to subtract a time interval from another), a
+    TimeSet is needed to track non-contiugous sets of time.
+
+    Internally, the TimeSet is just a list of TimeIntervals that exposes the
+    same operations as TimeInterval and is largely interoperable.
+    """
 
     def __init__(self, time_intervals: List[TimeInterval]):
         """Inits the TimeSet.
