f082517f9668dee877be8c7c949260320399b606
RyanDoesMath <rfolks92@gmail.com>
2025-04-22 16:18:12 -0400

    Add a test for compute_union with  a realistic set of TimeIntevals.

--- tests/test_time_set.py
+++ tests/test_time_set.py
@@ -437,3 +437,19 @@ def test_union_nested_timeintervals():
         TimeInterval(NOW - 2 * ONE_MINUTE, NOW + 2 * ONE_MINUTE),
     ]
     assert TimeSet(time_intervals).compute_union() == true_union
+
+
+def test_union_mixed_timeintervals():
+    """Tests the compute_union method with a relatively realistic group of TimeIntervals."""
+    time_intervals: List[TimeInterval] = [
+        TimeInterval(NOW - 2 * ONE_MINUTE, NOW + 2 * ONE_MINUTE),
+        TimeInterval(NOW + ONE_MINUTE, NOW + 3 * ONE_MINUTE),
+        TimeInterval(NOW + 4 * ONE_MINUTE, NOW + 5 * ONE_MINUTE),
+        TimeInterval(NOW - ONE_MINUTE, NOW),
+        TimeInterval(NOW - 3 * ONE_MINUTE, NOW - 2 * ONE_MINUTE),
+    ]
+    true_union: List[TimeInterval] = [
+        TimeInterval(NOW - 3 * ONE_MINUTE, NOW + 3 * ONE_MINUTE),
+        TimeInterval(NOW + 4 * ONE_MINUTE, NOW + 5 * ONE_MINUTE)
+    ]
+    assert TimeSet(time_intervals).compute_union() == true_union
