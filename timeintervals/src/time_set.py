d7dff98eb5a0c0c0c342a41cd442aff4b6fa576d
RyanDoesMath <rfolks92@gmail.com>
2025-04-22 15:33:39 -0400

    Add a subtract method that overloads '-'.

--- timeintervals/src/time_set.py
+++ timeintervals/src/time_set.py
@@ -1,6 +1,9 @@
 """A module defining the TimeSet class, a set of TimeIntervals."""
 
 from .time_interval import TimeInterval
+from datetime import datetime
+from functools import reduce
+from operator import add
 from typing import List, Optional, Union
 from typing_extensions import Self
 
@@ -48,30 +51,188 @@ class TimeSet:
         elif isinstance(other, TimeInterval):
             return TimeSet(self.time_intervals + [other])
         else:
-            raise ValueError(f"other is a {type(other)}, not a TimeSet or a TimeInterval.")
+            raise ValueError(
+                f"other is a {type(other)}, not a TimeSet or a TimeInterval."
+            )
 
     def __eq__(self, other: Self) -> bool:
         """Determines if this TimeSet is equal to the other by comparing their time_intervals."""
         return self.time_intervals == other.time_intervals
-    
+
     def __repr__(self) -> str:
         """An unambiguous string representation of this TimeSet."""
         str_time_intervals: str = [ti.__repr__() for ti in self.time_intervals]
         representation: str = f"TimeSet(time_intervals={str_time_intervals})"
         return representation
 
-    def __sub__(self, other: Union[Self, TimeInterval]) -> Self:
+    def __sub__(self, subtrahend: Union[Self, TimeInterval]) -> Self:
         """Implements set subtraction between this TimeInterval and another TimeInterval or Timeset.
 
         Args:
-            other (Union[Self, TimeSet]):
-                The other object. Either a TimeInterval or a TimeSet.
+            subtrahend (Union[Self, TimeSet]):
+                The other object to subtract from this one. Either a TimeInterval or a TimeSet.
+                (subtrahend is the right part of any subtraction, the left part is the minuend)
 
         Returns:
             A TimeInterval or a TimeSet, depending on the inputs. May result in an empty TimeSet.
         """
-        pass
-   
+        if isinstance(subtrahend, TimeSet):
+            return TimeSet._subtract_timeset_from_timeset(self, subtrahend)
+        elif isinstance(subtrahend, TimeInterval):
+            return TimeSet._subtract_timeinterval_from_timeset(self, subtrahend)
+        else:
+            raise ValueError(f"Cannot subtract type {type(subtrahend)} from a TimeSet.")
+
+    @staticmethod
+    def _subtract_timeset_from_timeset(
+        minuend: Self,
+        subtrahend: Self,
+    ) -> Self:
+        """Subtracts a TimeSet from a TimeSet.
+
+        This algorithm sorts the input time intervals and checks every loop if the resulting
+        difference is empty, or if the rest of the subtrahends are disjoint with the current
+        difference, and breaks the inner loop accordingly. This is slower for smaller TimeSets,
+        but provides a better efficiency for larger TimeSets.
+
+        Args:
+            minuend (TimeSet):
+                The TimeInterval being subtracted from.
+            subtrahend (TimeSet):
+                The TimeInterval being subtracted.
+
+        Returns:
+            A TimeSet containing the difference between the minuend and the subtrahend.
+        """
+        differences: List[TimeSet] = list()
+        sorted_minuend_time_intervals = sorted(
+            minuend.time_intervals, key=lambda ti: ti.start
+        )
+        sorted_subtrahend_time_intervals = sorted(
+            subtrahend.time_intervals, key=lambda ti: ti.start
+        )
+        for minuend_time_interval in sorted_minuend_time_intervals:
+            diff: TimeSet = TimeSet([minuend_time_interval])
+            for subtrahend_time_interval in sorted_subtrahend_time_intervals:
+                diff: TimeSet = TimeSet._subtract_timeinterval_from_timeset(
+                    diff,
+                    subtrahend_time_interval,
+                )
+                latest_end_in_diff: datetime = max(
+                    [ti.end for ti in diff.time_intervals],
+                    default=None,  # if the default is triggered, then diff is empty.
+                )
+                if (
+                    diff.is_empty()
+                    or subtrahend_time_interval.start > latest_end_in_diff
+                ):
+                    break
+            if not diff.is_empty():
+                differences += diff.time_intervals
+        return TimeSet(differences)
+
+    @staticmethod
+    def _subtract_timeinterval_from_timeset(
+        minuend: Self,
+        subtrahend: TimeInterval,
+    ) -> Self:
+        """Subtracts a TimeInterval from a TimeSet.
+
+        Args:
+            minuend (TimeSet):
+                The TimeInterval being subtracted from.
+            subtrahend (TimeInterval):
+                The TimeInterval being subtracted.
+
+        Returns:
+            A TimeSet containing the difference between the minuend and the subtrahend.
+        """
+        differences: List[TimeSet] = [
+            TimeSet._subtract_timeinterval_from_timeinterval(ti, subtrahend)
+            for ti in minuend.time_intervals
+        ]
+        print(f"Differences: {differences}")
+        differences: List[TimeSet] = list(
+            filter(lambda ts: not ts.is_empty(), differences)
+        )
+        return reduce(add, differences, TimeSet([]))
+
+    @staticmethod
+    def _subtract_timeinterval_from_timeinterval(
+        minuend: TimeInterval, subtrahend: TimeInterval
+    ) -> Self:
+        """Subtracts two TimeIntervals.
+
+        Args:
+            minuend (TimeInterval):
+                The TimeInterval being subtracted from.
+            subtrahend (TimeInterval):
+                The TimeInterval being subtracted.
+
+        Returns:
+            A TimeSet containing the difference between the minuend and the subtrahend.
+        """
+        if minuend.is_disjoint_with(subtrahend):
+            return TimeSet([minuend])
+        elif minuend.is_nested_in(subtrahend):
+            return TimeSet([])
+        elif subtrahend.is_nested_in(minuend):
+            return TimeSet._subtract_nested_timeintervals(minuend, subtrahend)
+        else:
+            return TimeSet._subtract_non_nested_timeintervals(minuend, subtrahend)
+
+    @staticmethod
+    def _subtract_nested_timeintervals(
+        minuend: TimeInterval, subtrahend: TimeInterval
+    ) -> Self:
+        """Subtracts timeintervals where the subtrahend is nested in the minuend.
+
+        Args:
+            minuend (TimeInterval):
+                The TimeInterval being subtracted from.
+            subtrahend (TimeInterval):
+                The TimeInterval being subtracted.
+
+        Returns:
+            A TimeSet containing the difference between the minuend and the subtrahend.
+        """
+        if minuend.start == subtrahend.start:
+            return TimeSet([TimeInterval(subtrahend.end, minuend.end)])
+        elif minuend.end == subtrahend.end:
+            return TimeSet([TimeInterval(minuend.start, subtrahend.start)])
+        else:
+            return TimeSet(
+                [
+                    TimeInterval(minuend.start, subtrahend.start),
+                    TimeInterval(subtrahend.end, minuend.end),
+                ]
+            )
+
+    @staticmethod
+    def _subtract_non_nested_timeintervals(
+        minuend: TimeInterval, subtrahend: TimeInterval
+    ) -> Self:
+        """Subtracts overlapping TimeIntervals that are not nested.
+
+        Args:
+            minuend (TimeInterval):
+                The TimeInterval being subtracted from.
+            subtrahend (TimeInterval):
+                The TimeInterval being subtracted.
+
+        Returns:
+            A TimeSet containing the difference between the minuend and the subtrahend.
+        """
+        if minuend.start < subtrahend.start:
+            return TimeSet([TimeInterval(minuend.start, subtrahend.start)])
+        elif minuend.start > subtrahend.start:
+            return TimeSet([TimeInterval(subtrahend.end, minuend.end)])
+        else:
+            # Shouldn't be possible unless this method is directly invoked on arbitrary inputs.
+            raise ValueError(
+                "Minuend and subtrahend are nested, but this method only handles non nested cases."
+            )
+
     def is_empty(self) -> bool:
         """Determines if this time interval is empty."""
         return len(self.time_intervals) == 0
