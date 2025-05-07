1fe1d3adfd675a204b02143429364a302832a5d3
RyanDoesMath <rfolks92@gmail.com>
2025-05-07 17:50:11 -0400

    Add the 'read the docs' theme and apply it.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ release = '0.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.napoleon", "sphinx.ext.autodoc"]
+extensions = ["sphinx.ext.napoleon", "sphinx.ext.autodoc", "sphinx_rtd_theme"]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -30,5 +30,5 @@ autodoc_mock_imports = ["pydantic", "typing_extensions"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
